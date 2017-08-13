a4ae30912856ca8da617a1baeae332bae68d6b35
ShainogaOlecsandr <ifqyjuf17@gmail.com>
2017-08-13 13:03:20 +0300

    satori->SparkStreaming Working

---
+++ SparkStreamingConsumer/src/main/java/Main.java
@@ -0,0 +1,47 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount");
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
+        Map<String, Object> props = new HashMap();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "consumer-tutorial");
+        props.put("key.deserializer", StringDeserializer.class.getName());
+        props.put("value.deserializer", StringDeserializer.class.getName());
+
+        Collection<String> topics = Arrays.asList("javaworld");
+
+        JavaInputDStream<ConsumerRecord<String, String>> stream =
+                KafkaUtils.createDirectStream(
+                        jssc,
+                        LocationStrategies.PreferConsistent(),
+                        ConsumerStrategies.<String, String>Subscribe(topics, props)
+                );
+
+//        stream.mapToPair(record -> new Tuple2<String, String>(record.key(), record.value())).foreachRDD(a -> System.out.print(a));
+        stream.map(a -> {
+            System.out.println(a.value());
+            return a.value();
+        }).print();
+        jssc.start();
+        jssc.awaitTermination();
+
+    }
+}
